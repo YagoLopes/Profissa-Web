fcf10721842351397d1176288eecdcbf80e0c1f7
Yago <yagolopeslazaro@gmail.com>
2018-12-13 11:35:55 -0200

    Desenvolvido no Projeto Integrador

---
+++ Assets/material/scripts/teste_vocacional.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class teste_vocacional : MonoBehaviour {
+	public void testeVocacional(string teste_vocacional){
+	Application.OpenURL("http://souvestibulando.com/teste_vocacional.php");
+	}
+
+}
---
+++ Assets/material/scripts/troca_tela.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class troca_tela : MonoBehaviour {
+	public void Teoca_tela(string nomeCena)
+	{
+
+		Application.LoadLevel(nomeCena);
+
+	}
+}
